8ea9e8c63b7bc6ee0e2297c2521d5f92bdf4f6c4
tusdasa <tusdasa@163.com>
2023-07-18 08:10:13 +0800

    update

--- src/cors/cors.php
+++ src/cors/cors.php
@@ -1,13 +1,20 @@
 <?php
 // 设置响应头
 header('Content-type: application/json');
-
-
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: GET,POST');
+header('Access-Control-Allow-Methods: POST,GET,OPTIONS,DELETE,GET');
+header('Access-Control-Allow-Headers: Accept,Content-Type,User-Agent');
 header('Access-Control-Allow-Credentials: true');
-
+header('Access-Control-Max-Age: 86400');
 $req_method = $_SERVER['REQUEST_METHOD'];
+
+if ($req_method == "OPTIONS") {
+	 http_response_code('204');
+	 echo "";
+	 return;
+}
+
+
 echo json_encode(
 	[
 				"code"    =>  200,
